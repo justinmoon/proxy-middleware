87f98afd5585e89dc3e6284c8c51735efed9ecac
Justin Moon <mail@justinmoon.com>
2022-03-01 17:54:26 +0000

    Form to dynamically add route

--- index.js
+++ index.js
@@ -1,77 +1,91 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require('body-parser')
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 // Create Express Server
 const app = express();
+var router = undefined
+var routes = {
+  "/contacts": "https://contacts.superquest.repl.co",
+  // "/notes": "https://notes.superquest.repl.co",
+}
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Configuration
 const PORT = 3000;
-const HOST = "localhost";
-const CONTACTS_URL = "https://contacts.superquest.repl.co";
-const NOTES_URL = "https://notes.superquest.repl.co";
+const HOST = "0.0.0.0";
 
 // Logging
 app.use(morgan("dev"));
 
-// Info GET endpoint
-app.get("/info", (req, res, next) => {
-  res.send("This is a proxy service which proxies to JSONPlaceholder API.");
-});
+// this should be the only thing on your app
+app.use(function(req, res, next) {
+  // this needs to be a function to hook on whatever the current router is
+  router(req, res, next)
+})
 
-// Authorization
-// app.use('', (req, res, next) => {
-//     if (req.headers.authorization) {
-//         next();
-//     } else {
-//         res.sendStatus(403);
-//     }
-// });
+function updateRouter() {
+  router = express.Router()
 
-// Notes
-app.use(
-  "/notes",
-  createProxyMiddleware({
-    target: CONTACTS_URL,
-    changeOrigin: true,
-    pathRewrite: {
-      [`^/notes`]: "",
-    },
-  })
-);
+  for (const [from, to] of Object.entries(routes)) {
+    console.log(`${from} -> ${to}`)
+    app.use(
+      from,
+      createProxyMiddleware({
+        target: to,
+        changeOrigin: true,
+        pathRewrite: {
+          [`^${from}`]: "",
+        },
+      })
+    );
+  }
 
-// Contacts app
-app.use(
-  "/contacts",
-  createProxyMiddleware({
-    target: CONTACTS_URL,
-    changeOrigin: true,
-    pathRewrite: {
-      [`^/contacts`]: "",
-    },
-  })
-);
+  // Hack for next.js
+  app.use(
+    "/_next/*",
+    createProxyMiddleware({
+      target: "/",
+      changeOrigin: true,
+      router: (req) => {
+        const referrer = req.get("Referrer");
+        if (!referrer) {
+          throw new Error("Need referrerer");
+        }
+        for (const [from, to] of Object.entries(routes)) {
+          if (referrer.includes(from)) {
+            return to;
+          }
+        }
+      },
+    })
+  );
 
-// Route next assets depending on referrer (might be better way to do this)
-app.use(
-  "/_next/*",
-  createProxyMiddleware({
-    target: CONTACTS_URL,
-    changeOrigin: true,
-    router: (req) => {
-      const referrer = req.get("Referrer");
-      if (!referrer) {
-        throw new Error("Need referrerer");
-      }
-      if (referrer.includes("/contacts")) {
-        return CONTACTS_URL;
-      }
-      if (referrer.includes("/notes")) {
-        return NOTES_URL;
-      }
-    },
-  })
-);
+  // app.get("/", (req, res, next) => {
+  //   res.send("<a href='/notes'>Notes</a> | <a href='/contacts'>Contacts</a>");
+  // });
+
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  app.post('/', urlencodedParser, (req, res) => {
+    const to = req.body.url
+    const from = req.body.url.slice(8) // chop of "https://"
+    routes["/" + from] = to
+    
+    console.log("updated routes", routes)
+    
+    // TODO: persist this change
+    updateRouter()
+    
+    // console.log('Got body:', req.body);
+    res.redirect(from);
+  });
+}
+
+updateRouter()
 
 // Start Proxy
 app.listen(PORT, HOST, () => {
